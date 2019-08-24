11cc7edf3b018fc5da832cd05420ce29c23f3e7b
peakyDicers <ian.chui@hotmail.com>
2019-08-24 13:25:01 -0400

    thing

--- src/Zillow.js
+++ src/Zillow.js
@@ -10,24 +10,46 @@ export default class Zillow extends React.Component{
     const axios = require('axios');
 
     // Make a request for a user with a given ID
-    var config = {
-      headers: {'Access-Control-Allow-Origin': '*'}
-  };
-    axios.get('http://www.zillow.com/webservice/GetZestimate.htm?zws-id=<X1-ZWz1hb9u92p3pn_a80zd>&zpid=48749425', config)
-      .then(function (response) {
-        // handle success
-        console.log(response);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .finally(function () {
+  //   var config = {
+  //     headers: {'Access-Control-Allow-Origin': '*'}
+  // };
+  //   axios.get('http://www.zillow.com/webservice/GetZestimate.htm?zws-id=<X1-ZWz1hb9u92p3pn_a80zd>&zpid=48749425', { crossdomain: true })
+  //     .then(function (response) {
+  //       // handle success
+  //       console.log(response);
+  //     })
+  //     .catch(function (error) {
+  //       // handle error
+  //       console.log(error);
+  //     })
+  //     .finally(function () {
 
-      });
+  //     });
+    console.log("lmao")
+      fetch("https://crossorigin.me/https://www.zillow.com/webservice/GetZestimate.htm?zws-id=<X1-ZWz1hb9u92p3pn_a80zd>&zpid=48749425")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+          console.log('askjdlkasjd')
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
   render(){
+    console.log(this.state)
     return(
       <div>
       <input></input>
