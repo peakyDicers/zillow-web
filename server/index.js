144d1a9fcdf37cf3be443b15f70004bcdb168db3
peakyDicers <ian.chui@hotmail.com>
2019-08-24 15:08:02 -0400

    added server.

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+
+var parseString = require('xml2js').parseString;
+
+
+const port = 3000
+let key = 'X1-ZWz1hb9u92p3pn_a80zd'
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/getZillow', (req, res) => {
+  let addr =  req.query.addr;
+  let cityStateZip = req.query.cityStateZip;
+  // let testAddr = '2114+Bigelow+Ave';
+  // let testCitystatezip = 'Seattle%2C+WA'
+  let testAddr = '14500+Sylena+Way';
+  let testCitystatezip = 'OklahomaCity%2C+OK'
+  
+  axios(`http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=${key}&address=${testAddr}&citystatezip=${testCitystatezip}`, {
+    method: 'GET',
+    mode: 'no-cors',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+    credentials: 'same-origin',
+  }).then(data => {
+    //print xml.
+    //console.log(data.data)
+    let xml = data.data;
+    parseString(xml, function (err, result) {
+      let money = result["SearchResults:searchresults"].response[0].results[0].result[0].zestimate[0].amount[0]._;
+      console.log(money);
+      res.send(money);
+    });
+  })
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- src/Zillow.js
+++ src/Zillow.js
@@ -3,58 +3,45 @@ import React from 'react';
 export default class Zillow extends React.Component{
   constructor(props){
     super(props)
-
+    console.log("assad")
+    this.state = {
+      money: 0,
+      currency: 'USD'
+    }
   }
 
   getData = () => {
     const axios = require('axios');
-
-    // Make a request for a user with a given ID
-  //   var config = {
-  //     headers: {'Access-Control-Allow-Origin': '*'}
-  // };
-  //   axios.get('http://www.zillow.com/webservice/GetZestimate.htm?zws-id=<X1-ZWz1hb9u92p3pn_a80zd>&zpid=48749425', { crossdomain: true })
-  //     .then(function (response) {
-  //       // handle success
-  //       console.log(response);
-  //     })
-  //     .catch(function (error) {
-  //       // handle error
-  //       console.log(error);
-  //     })
-  //     .finally(function () {
-
-  //     });
-    console.log("lmao")
-      fetch("https://crossorigin.me/https://www.zillow.com/webservice/GetZestimate.htm?zws-id=<X1-ZWz1hb9u92p3pn_a80zd>&zpid=48749425")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result.items
-          });
-          console.log('askjdlkasjd')
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+    axios.get('http://localhost:3000/getZillow')
+      .then((response) => {
+        // handle success
+        console.log(response);
+        this.setState({money: response.data})
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
   }
 
   render(){
     console.log(this.state)
     return(
       <div>
+        <div>
+        <input></input>
+        </div>
+        <div>
+        <input></input>
+        </div>
+        <button>go</button>
       <input></input>
       <button onClick={this.getData}></button>
       <h1>HELLLLLLO THERE</h1>
+      <h1>{`$ ${this.state.money} USD`}</h1>
       </div>
     )
   }
