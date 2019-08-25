be74ecec5e436f8bc827d3b41a756f39fede53b4
Kindeep <kk17xg@brocku.ca>
2019-08-25 04:01:34 -0400

    idk man theres some shit here

--- src/Zillow.js
+++ src/Zillow.js
@@ -90,6 +90,26 @@ export default class Zillow extends React.Component {
     setTimeout(this.uploadCompleted, 3000, 'funky');
   }
 
+  getDataUri(url, callback) {
+  var image = new Image();
+
+  image.onload = function () {
+    var canvas = document.createElement('canvas');
+    canvas.width = this.naturalWidth; // or 'width' if you want a special/scaled size
+    canvas.height = this.naturalHeight; // or 'height' if you want a special/scaled size
+
+    canvas.getContext('2d').drawImage(this, 0, 0);
+
+    // Get raw image data
+    callback(canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, ''));
+
+    // ... or get as Data URI
+    callback(canvas.toDataURL('image/png'));
+  };
+
+  image.src = url;
+}
+
   imgSelected = (e) => {
 
     this.setState({ file: URL.createObjectURL(e.target.files[0]) });
