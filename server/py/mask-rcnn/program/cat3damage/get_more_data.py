63a541308e498bd97a02d7cdc1444a070d645151
Kindeep <kk17xg@brocku.ca>
2019-08-25 06:18:49 -0400

    image works

--- server/py/mask-rcnn/program/cat3damage/get_more_data.py
+++ server/py/mask-rcnn/program/cat3damage/get_more_data.py
@@ -1,25 +1,33 @@
-# some_file.py
-# import sys
-# import os
-
-# insert at 1, 0 is the script path (or '' in REPL)
-# sys.path.insert(1, './Mask-RCNN - Intact Insurance/program/cat3damage/')
-# sys.path.insert(1, './Mask-RCNN - Intact Insurance/')
+from PIL import Image, ImageDraw
+import base64
+import io
 
 import json
-# os.chdir('./Mask-RCNN - Intact Insurance/program/cat3damage/')
-from return_data import *
+import sys
+
+def imgData(img) -> str:
+    buffer = io.BytesIO()
+    img.save(buffer, format='PNG')
+    buffer.seek(0)
+
+    data_uri = base64.b64encode(buffer.read()).decode('ascii')
+    return '<img src="data:image/png;base64,{0}">'.format(data_uri)
 
 class DummyFile(object):
     def write(self, x): pass
 
-save_stdout = sys.stdout
-sys.stdout = DummyFile()
-# print("Running ret_me")
-dictr = ret_me2(sys.argv[1])
-# print(dictr)
-sys.stdout = save_stdout
-# print(dictr)
-# print("Something should have printed by now...")
+# save_stdout = sys.stdout
+# sys.stdout = DummyFile()
 
-print(json.dumps(dictr))
+from return_data import *
+
+dictr = ret_me2(sys.argv[1])
+print("Wut")
+
+result = {
+    "houses": dictr,
+    "marked_image": imgData(Image.open("output.png")) if len(sys.argv) > 0 else None
+}
+
+# sys.stdout = save_stdout
+print(json.dumps(result))
